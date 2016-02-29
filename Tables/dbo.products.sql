dd05dc643e07bf2f2b828f7d6c8997c305336d18
anonymous <anonymous@example.com>
2016-02-29 09:11:34 +0000

    add products table

---
+++ Tables/dbo.products.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[products]
+(
+[name] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[price] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[number in stock] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+)
+GO
