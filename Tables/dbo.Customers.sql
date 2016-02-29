a05a03d7528e37a2d461870cc18569afc739bd29
anonymous <anonymous@example.com>
2016-02-29 09:10:14 +0000

    add credit card to customers table

--- Tables/dbo.Customers.sql
+++ Tables/dbo.Customers.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[Customers]
 (
 [username] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [email address] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
-[password] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+[password] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[credit card] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 )
 GO
