4fa1512af686e15ee92eb3606b7f56d4883fb65d
anonymous <anonymous@example.com>
2016-02-29 09:14:30 +0000

    add customer rating

--- Tables/dbo.products.sql
+++ Tables/dbo.products.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[products]
 (
 [name] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [price] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
-[number in stock] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+[number in stock] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[customer rating] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 )
 GO
