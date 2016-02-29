9f1279ff8773181c6ad858638228950533aa6908
anonymous <anonymous@example.com>
2016-02-29 09:09:43 +0000

    add password to customers table

--- Tables/dbo.Customers.sql
+++ Tables/dbo.Customers.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Customers]
 (
 [username] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
-[email address] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+[email address] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[password] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 )
 GO
